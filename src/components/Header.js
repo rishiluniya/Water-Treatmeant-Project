d1384628033ecd463d367d9ea74a935bc68bee4d
ShamitrM <shamitr.mardikar@gmail.com>
2022-08-26 20:28:50 +0530

    Test

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+function Header(){
+    return(
+        <div>
+            <div>
+                <h1 className='Header'>Water Billing System</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;

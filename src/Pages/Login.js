ab4cbb8f2ae37daa4bad80d8aa82ea8f12f11906
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-08-27 01:41:19 +0530

    asdad

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button2 from '../components/Button2';
 function Login(){
     return(
         <div>
             <Header />
+            <Button2 />
         </div>
     );
 }

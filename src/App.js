20275c19e475bcec085382a67ac87218364d3e21
ShamitrM <shamitr.mardikar@gmail.com>
2022-08-26 21:27:32 +0530

    Push Button Del

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Header from './components/Header';
-import button from './components/button';
 function App() {
   return (
     <div >
       <Header />
-      <button />
-       
     </div>
   );
 }

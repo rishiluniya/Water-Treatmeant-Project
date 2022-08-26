f98055396e39510fcc0624e03b1843fa11b2ed3b
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-08-26 23:10:56 +0530

    sdas

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,10 +6,12 @@ function Info() {
         <div>
 
                 <div className='Info'>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto est blanditiis quia dolore magnam corrupti minima veritatis culpa nostrum repellat soluta, inventore nam quis ea quos nobis molestiae, perspiciatis quod.
-                    Libero, aliquam? Similique molestias qui facilis maiores, ratione error dicta vitae reiciendis minus provident eligendi? Fugit unde minus aperiam rerum delectus nostrum quos, ipsa quas id, quia, ea provident? Aliquam.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto est blanditiis quia dolore magnam corrupti minima veritatis culpa nostrum repellat soluta, inventore nam quis ea quos nobis molestiae, perspiciatis quod.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    <p>
+                    This website is dedicated to provide the user information about their current water usage. You will be provided with the amount of water used, along with the corresponding amount that you are expected to pay. Failure to do so may incur additional charges or more. You are to pay online using our portal by any means including credit card, debit card or upi. Please login using your credentials by clickng on the login on the botttom right of the screen which will redirect you to the login screen.
+                    </p>
+                    <p>
+                    The National Water Development Agency (NWDA), was set up in July 1982 as Autonomous Society under the societies registration act 1860, to carry out the water balance and other studies on a scientific and realistic basis for optimum utilization of water resources of the Peninsular river system for preparation of feasibility reports and thus to give concrete shape to Peninsular river development component of National perspective plan prepared by Central Water Commission and the then Ministry of Irrigation (now Ministry of Jal Shakti, Department of Water Resources, River Development & Ganga Rejuvenation(DoWR, RD& GR)). In the year 1990, NWDA was also entrusted with the task of Himalayan Component of National Perspective Plan. In 2006, it was decided that NWDA will explore the feasibility of intra-states links and to take up the work for preparation of Detailed Project Report (DPR) of river link proposals under National Perspective Plan (NPP).
+                    </p>
                 </div>
 
         </div>

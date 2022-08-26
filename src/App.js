a5eaf6c5e4db029a3f86c5773e4cf118edba26c6
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-08-27 00:13:31 +0530

    sad

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Header from './components/Header';
-import Info from './components/Info';
-
-import Button from './components/Button';
+import Login from './Pages/Login';
+import { Route, Routes } from 'react-router-dom';
+import React, { Component }  from 'react';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div >
-      <Header />
-      <Info />
-      <Button />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Button.css';
 
 function Button() {
     return (
         <div className="cust-button">
-            LOGIN
+            <Link to="/login" className='link'>LOGIN</Link>
         </div>
     );
 }

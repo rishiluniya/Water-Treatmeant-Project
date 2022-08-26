b099a7444df00302d68dab7a2450d56de614ee48
ShamitrM <shamitr.mardikar@gmail.com>
2022-08-27 01:59:58 +0530

    login page

--- src/components/Loginwindow.js
+++ src/components/Loginwindow.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Loginwindow.css';
+import Photu from '../Ui/source.png'
+
 function Loginwindow(){
     return(
         <div>
             
             <div className='Rect'>
-            <div></div>
+                <div className='Image'>
+                <img src={Photu} />
+                </div>
+                <div className='Form'>
+                <form action="" method="" className='Form_maybe'>
+                    <label for="name">Username : </label>
+                    <input type="text" name="name" id="name" required size={20}></input>
+                </form>
+                <div className='pass'>
+                <form action="" method="" className='Form_maybe'>
+                    <label for="name">Password : </label>
+                    <input type="text" name="name" id="name" required size={20}></input>
+                </form>
+                </div>
+                </div>
+
             </div>
         </div>
     );

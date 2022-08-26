8b6d9cf655730ff47fa1988ef9a66d5837a22102
ShamitrM <shamitr.mardikar@gmail.com>
2022-08-27 01:31:14 +0530

    loginwindow commit

---
+++ src/components/Loginwindow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Loginwindow.css';
+function Loginwindow(){
+    return(
+        <div>
+            
+            <div className='Rect'>
+            <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Loginwindow;

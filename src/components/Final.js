d0dc5ea81411f4fa914859469865c0ea59c675bd
saakshi-ritolia <104695735+saakshi-ritolia@users.noreply.github.com>
2022-08-27 03:33:04 +0530

    Update Final.js

--- src/components/Final.js
+++ src/components/Final.js
@@ -4,15 +4,15 @@ function Final() {
     return (
         <div>
             <div className="allInfo" >
-                <h2>Name: &nbsp;&nbsp;&nbsp;&nbsp;John Paul Martin</h2>
-                <h2>Sex:&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;  Twice a Day</h2>
-                <h2>Reg No:&nbsp;&nbsp; 69696969</h2>
-                <h2>Locality:&nbsp;&nbsp; Your Mom's House</h2>
-                <h2>Litres Used: &nbsp;&nbsp;69.420</h2>
+                <h2>Name: &nbsp;&nbsp;&nbsp;&nbsp;Raj kumar</h2>
+                <h2>Sex:&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;  Male</h2>
+                <h2>Reg No:&nbsp;&nbsp; 432567</h2>
+                <h2>Locality:&nbsp;&nbsp; Marathali Bangalore</h2>
+                <h2>Litres Used: &nbsp;&nbsp;111</h2>
             </div>
             <div className='margin-top'>
                 <h1 className="firstPart">The Amount to be Paid is: </h1>
-                <h1 className="secondPart">Rehne de tera ghar jayega</h1>
+                <h1 className="secondPart">Rs 453/-</h1>
             </div>
         </div>
     );
